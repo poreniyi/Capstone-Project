4373faec3188c4968d4c314cef0ff5fa3a6d7d87
Poreniyi <poreniyi@gmail.com>
2021-02-26 15:18:50 -0500

    added middleware to catch incoming form data

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path')
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+})
+
+app.post('/results', function(req, res) {
+    console.log(req.body);
     res.sendFile(path.join(__dirname + '/home.html'));
 })
 app.set("port", process.env.PORT || 8081);

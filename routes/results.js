1e20deaa7250887597d9ef9995491de5297acbe5
BuildTools <barrettrose1@gmail.com>
2021-02-27 01:51:29 -0500

    Added form data to Rendeview class functionality

--- routes/results.js
+++ routes/results.js
@@ -1,6 +1,11 @@
 let router = require('express').Router();
+const Rendeview = require('../rendeview.js');
 
 router.post('/results', function(req, res) {
+    var locationTextField = req.body.location;
+    var test = new Rendeview(locationTextField);
+    console.log(test.debugPrint());
+
     res.render('results',{data:req.body});
 })
 

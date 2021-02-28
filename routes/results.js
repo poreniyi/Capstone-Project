606426b68fbe622083f6dab7cae0411f1478bd00
Poreniyi <poreniyi@gmail.com>
2021-02-28 12:49:33 -0500

    made post route using rendeview2

--- routes/results.js
+++ routes/results.js
@@ -1,19 +1,30 @@
 let router = require('express').Router();
 const Rendeview = require('../rendeview.js');
+const Rendeview2 = require('../rendeview2.js');
 
-router.post('/results', function(req, res) {
+// router.post('/results', function(req, res) {
+//     var locationTextField = req.body.location;
+//     var test = new Rendeview(locationTextField);
+//     var coordinates = [];
+//     test.exportCoordinates().then(exportData=>{
+//         coordinates = exportData;
+//         console.log(coordinates);
+//         res.render('results',{data:req.body, 
+//                               centerpointLat:coordinates[0],
+//                               centerpointLng:coordinates[1]
+//                             });
+//     })
+// })
+
+router.post('/results', async(req,res)=>{
     var locationTextField = req.body.location;
-    var test = new Rendeview(locationTextField);
+    var test = new Rendeview2(locationTextField);
     var coordinates = [];
-
-    test.exportCoordinates().then(exportData=>{
-        coordinates = exportData;
-        console.log(coordinates);
-        res.render('results',{data:req.body, 
-                              centerpointLat:coordinates[0],
-                              centerpointLng:coordinates[1]
-                            });
-    })
+    console.log(apiData)
+    let apiData=await test.exportCoordinates();
+    res.render('results',{
+        data:req.body,
+        centerpointLat:apiData[0],
+        centerpointLng:apiData[1] })
 })
-
 module.exports=router;

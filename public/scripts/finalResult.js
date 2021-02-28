97e1174b83de4a0c21d7e6e6e1e82798ebf96445
Poreniyi <poreniyi@gmail.com>
2021-02-28 11:52:00 -0500

    moved map creation to seperate js file

---
+++ public/scripts/finalResult.js
@@ -0,0 +1,26 @@
+"use strict";
+
+        // Passed data through ejs render somehow converted data into string, must parseFloat 
+       let centerpointLat= parseFloat(document.getElementById('latCenter').textContent);
+       let centerpointLng= parseFloat(document.getElementById('lngCenter').textContent);
+
+        function initMap() {
+          const myLatLng = {
+            lat: centerpointLat,
+            lng: centerpointLng
+          };
+          const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 13,
+            center: myLatLng,
+            fullscreenControl: false,
+            zoomControl: true,
+            mapTypeControl: false,
+            streetViewControl: false
+          });
+          new google.maps.Marker({
+            position: myLatLng,
+            map,
+            title: "Hello World!"
+          });
+        }
+console.log(`I am being found`);

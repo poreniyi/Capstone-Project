790e61817aaa7c4883ba2a61d03e7ad7eddbcd1a
Poreniyi <poreniyi@gmail.com>
2021-03-01 11:02:18 -0500

    added functions to filter out empty strings and trimming

--- routes/results.js
+++ routes/results.js
@@ -18,6 +18,9 @@ const Rendeview2 = require('../rendeview2.js');
 
 router.post('/results', async(req,res)=>{
     var locationTextField = req.body.location;
+    locationTextField=locationTextField.filter(location=>location);
+    locationTextField=locationTextField.map(location=>location.trim());
+    console.log(req.body);
     var test = new Rendeview2(locationTextField);
     let apiData=await test.exportCoordinates();
     centerpoint=apiData.centerpointCoordinates;

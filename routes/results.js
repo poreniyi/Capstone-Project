b884b4a1c9702d2c1611b61d69b79a7d2b8866d4
Poreniyi <poreniyi@gmail.com>
2021-03-03 18:51:51 -0500

    changed passing of req.body.locations to just an array also added a check that sends useer back to homepage if no locations

--- routes/results.js
+++ routes/results.js
@@ -20,22 +20,23 @@ router.post('/results', async(req,res)=>{
     var locationTextField = req.body.location;
     locationTextField=locationTextField.filter(location=>location);
     locationTextField=locationTextField.map(location=>location.trim());
-    console.log(req.body);
+    if(!locationTextField.length) res.redirect('/')
+    console.log(locationTextField);
+    console.log(typeof locationTextField);
     var test = new Rendeview2(locationTextField);
     let apiData=await test.exportCoordinates();
     centerpoint=apiData.centerpointCoordinates;
     res.locals.loc=apiData.locationCoordinates;
     res.render('results',{
-        data:req.body,
+        locations:locationTextField,
         centerpoint,
          })
 })
 
 router.get('/test',async(req,res)=>{
-    let places=['KSU Marietta','KSU Kennesaw'];
-    let location={location:places};
+    let locations=['KSU Marietta','KSU Kennesaw'];
     res.locals.loc=[[33.9408531,-84.5204241],[34.0381785,-84.5826712]];
     let centerpoint=[33.9895158,-84.55154765];
-    res.render('results',{data:location,centerpoint});
+    res.render('results',{locations,centerpoint});
 })
 module.exports=router;

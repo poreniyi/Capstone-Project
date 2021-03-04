7febd3874b9b1af8b5bf28b5f723fa977082775f
Poreniyi <poreniyi@gmail.com>
2021-03-03 19:50:16 -0500

    added an event on click that adds an address and name text input and also a span to delete those inputs

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,1 +1,37 @@
 let addButton=document.getElementById('add');
+let form=document.getElementById('form');
+let submit=document.getElementById('submit');
+
+
+addButton.addEventListener('click',()=>{
+    let inputCount=document.getElementsByClassName('addressInfo').length;
+    if(inputCount>9){
+        console.log(`too many addresses`);
+        return;
+    }
+    console.log(inputCount);
+    let newDiv=document.createElement('div');
+    newDiv.classList.add('addressInfo');
+    let addressLabel=document.createElement('label');
+    addressLabel.textContent=`Location:${inputCount+1}`
+    let nameLabel=document.createElement('label');
+    nameLabel.textContent='Name'
+    let newLocation=  document.createElement('input');
+    newLocation.type='text';
+    newLocation.name='location';
+    newLocation.style.width='50%'
+    let close= document.createElement('span');
+    close.textContent='x';
+    let PersonsName=document.createElement('input');
+    PersonsName.name='Name';
+    close.addEventListener('click',()=>{
+        close.parentElement.remove(); 
+    });
+    newDiv.appendChild(addressLabel);
+    newDiv.appendChild(newLocation);
+    newDiv.appendChild(nameLabel);
+    newDiv.appendChild(PersonsName);
+    newDiv.appendChild(close);
+    form.insertBefore(newDiv,submit);
+})
+

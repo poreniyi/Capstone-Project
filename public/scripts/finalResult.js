228dafcca7f243e19f703357df2e27938569e3ab
Poreniyi <poreniyi@gmail.com>
2021-03-03 20:43:55 -0500

    added a radius of 5miles to the map

--- public/scripts/finalResult.js
+++ public/scripts/finalResult.js
@@ -34,6 +34,16 @@ function initMap() {
   centerpointMarker.addListener('mouseout', () => {
     centerpoint.style.backgroundColor=''
   })
+  const radius=new google.maps.Circle({
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    map,
+    center: myLatLng,
+    radius: 8046,//in meters
+})
   for (let i = 0; i < locInfo.length; i++) {
     let info = locInfo[i];
     let coordinates = info.querySelector('.coordiantes').textContent.split(',');
